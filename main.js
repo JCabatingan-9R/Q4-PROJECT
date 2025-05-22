9b4635e5dce1ad9e712bf26055a51e1b6ddd01e0
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2025-05-22 19:58:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 function ratingSubmit() {
   var rate = document.getElementById('ratings').value;
+  var name = document.getElementById('nameID').value;
   if (rate = "1") {
-    window.alert('Thanks for rating...we will get back to you soon. Sorry for the inconvenience >o<');
+    window.alert('Thanks for rating, ' + name + '...Sorry for the inconvenience >o<');
   }
+  else if (rate = "2") {
+    window.alert('Thanks for rating, ' + name + '...At least it is not a one >o<');
+  }
+
+  else if (rate = "3") {
+    window.alert('Thanks for rating, ' + name + '! Lucky number 3 >o<');
+  }
+
+  else if (rate = "4") {
+    window.alert('Thanks for rating, ' + name + '! Shucks, so close to a perfect >o<');
+  }
+
+  else if (rate = "5") {
+    window.alert('Thanks for rating, ' + name + '! We will remember you! >o<');
+  }
+    
   else {
     window.alert('Oops! Looks like you forgot to rate our site!');
   }
