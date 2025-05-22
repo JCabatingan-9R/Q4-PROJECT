0fe69be713e3a9cea56c794c4b89b7fca261cfb0
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2025-05-22 20:41:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 function ratingSubmit() {
-  var rate = document.getElementById('ratings').value;
-  var name = document.getElementById('fnameID').value;
-  if (rate = "1") {
+  var infoRate = document.getElementById('infoID').value;
+  var projRate = document.getElementById('projID').value;
+  var feedRate = document.getElementById('feedID').value;
+  var aveRate = Math.round((infoRate + projRate + feedRate) / 3);
+  if (aveRate <= 1) {
     window.alert('Thanks for rating, ' + name + '...Sorry for the inconvenience >o<');
   }
-  else if (rate = "2") {
+  else if (aveRate >= 2 && aveRate < 3) {
     window.alert('Thanks for rating, ' + name + '...At least it is not a one >o<');
   }
 
-  else if (rate = "3") {
-    window.alert('Thanks for rating, ' + name + '! Lucky number 3 >o<');
+ else if (aveRate >= 3 && aveRate < 4) {
+    window.alert('Thanks for rating, ' + name + '...Lucky in the middle! >o<');
   }
 
-  else if (rate = "4") {
-    window.alert('Thanks for rating, ' + name + '! Shucks, so close to a perfect >o<');
+ else if (aveRate >= 4 && aveRate < 5) {
+    window.alert('Thanks for rating, ' + name + '...Nearly perfect >o<');
   }
 
-  else if (rate = "5") {
-    window.alert('Thanks for rating, ' + name + '! We will remember you! >o<');
+ else if (aveRate = 5) {
+    window.alert('Thanks for rating, ' + name + '...We will remember you >o<');
   }
-    
+
   else {
     window.alert('Oops! Looks like you forgot to rate our site!');
   }
