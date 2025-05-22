d1ed871f25aadd4db28ca372fac0d4babb1a9e17
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2025-05-22 16:31:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function ratingSubmit() {
   if (rate = 'one') {
     window.alert('Thanks for rating...we will get back to you soon. Sorry for the inconvenience >o<');
   }
+
+function buttonChange() {
+}
+  
+function buttonReset() {
+}
